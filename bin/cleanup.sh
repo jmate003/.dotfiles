5f08bcacdfa05af7d596aad46c47662a6b3c05ae
Justin Mateo <justmateo0903@gmail.com>
2023-04-05 17:03:27 -0400

    Created linux shell setup scripts.

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cd
-rm ~/.vimrc #remove .vimrc from the home directory
-sed -i 's#source ~/.dotfiles/bashrc_custom#g' ~/.bashrc #find the line and replace it
-rm -r ~/.TRASH #remove the .TRASH directory
+rm -f ~/.vimrc #remove .vimrc from the home directory
+# sed -i 's///g'
+sed -i 's/source \~\/\.dotfiles\/etc\/bashrc\_custom//g' ~/.bashrc
+#sed -i 's#source ~/.dotfiles/bashrc_custom##g' ~/.bashrc #find the line and replace it
+rm -f -r ~/.TRASH  #remove the .TRASH directory
--- bin/linux.sh
+++ bin/linux.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 unamestr=$(uname)
-if [$unamestr == 'Linux']
+if [[ "$unamestr" == *"Linux"* ]];
 then 
 	# makes trash directory
-	cd
 	mkdir -p ~/.TRASH
-	if[~/.vimrc] #if the file exists execute
+	if [[ -f ~/.vimrc ]]; #if the file exists execute
 	then
-		mv .vimrc .bupvimrc #rename .vimrc to .bupvimrc
+		mv ~/.vimrc .bupvimrc #rename .vimrc to .bupvimrc
 		echo "The current .vimrc file was changed to '.bup_vimrc'" >> linuxsetup.log
 		cp -f ./etc/vimrc ~/.vimrc #copy the file to the new location
 		echo "source ~/.dotfiles/etc/bashrc_custom" >> ~/.bashrc
-
+	fi
 
 else #if linux is not the operating system
 	echo "Error" >>  linuxsetup.log
 	exit
-fi #finish
+fi #finishi
